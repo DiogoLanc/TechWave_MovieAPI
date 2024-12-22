3db820cb0f1dfa3100689261c0d3f478b64d5f30
DiogoLanca <fc53495@alunos.fc.ul.pt>
2024-12-22 16:55:44 +0000

    list all movies (GET) and add movie (POST) working

--- movie-api/src/movies/movies.controller.ts
+++ movie-api/src/movies/movies.controller.ts
@@ -2,16 +2,16 @@ import { Controller, Get, Post, Body } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './movie';
 
-@Controller('movies')
+@Controller('movies') // base route
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
-  @Get()
+  @Get() // list all movies
   async listMovies(): Promise<Movie[]> {
     return this.moviesService.listMovies();
   }
 
-  @Post()
+  @Post() // add movie to the database
   async addMovie(@Body() movieData: Partial<Movie>): Promise<Movie> {
     return this.moviesService.addMovie(movieData);
   }
--- movie-api/src/movies/movies.module.ts
+++ movie-api/src/movies/movies.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { MoviesController } from './movies.controller';
@@ -6,7 +7,7 @@ import { Movie } from './movie';
 import { Genre } from '../genres/genre';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Movie, Genre])], // Register Movie entity here
+  imports: [TypeOrmModule.forFeature([Movie, Genre])], 
   providers: [MoviesService],
   controllers: [MoviesController],
 })
