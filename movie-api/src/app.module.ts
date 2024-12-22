9c8eee38bb7a95fb1aea02e85fafa37ed5b95f3e
DiogoLanca <fc53495@alunos.fc.ul.pt>
2024-12-22 23:17:39 +0000

    protect db

--- movie-api/src/app.module.ts
+++ movie-api/src/app.module.ts
@@ -1,26 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MoviesModule } from './movies/movies.module';
 import { GenresModule } from './genres/genres.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123',
-      database: 'movie_db',
-      autoLoadEntities: true,
-      synchronize: true,
+    // Load environment variables
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes config available globally
+    }),
+    // Configure TypeORM with environment variables
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     MoviesModule,
     GenresModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
